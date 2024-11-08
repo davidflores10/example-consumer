575691271c36dde62f2fc4b28b645d2bbeef50d9
dfloresleroy <david.flores@ext.leroymerlin.es>
2024-11-08 09:02:48 +0100

    test new var in branch 2

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -20,7 +20,7 @@ describe("API Pact test", () => {
         id: "10",
         type: "CREDIT_CARD",
         name: "28 Degrees",
-        year: 28,
+        year: 29,
       };
 
       // Uncomment to see this fail
